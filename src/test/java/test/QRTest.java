a376e081304dcb0ddb8ac7cbf2ead86d020099eb
Lea <laitenberger@mail.hs-ulm.de>
2021-03-25 16:49:54 +0100

    QR Code Scanner
    my code for the qr code reader, not perfect but it works :)

--- src/test/java/test/QRTest.java
+++ src/test/java/test/QRTest.java
@@ -3,20 +3,30 @@ package test;
 import static org.junit.Assert.*;
 
 import java.io.File;
+import java.io.IOException;
+import java.net.URLDecoder;
 
+import java.net.URL;
+import maven.QRCodeReader;
 import org.junit.Test;
 
 public class QRTest {
 
-	@Test
-	public void test() {
-		String resourceName = "qr.png";
+    @Test
+    public void test() throws Exception {
+        String resourceName = "qr.png";
+        URL resource = ClassLoader.getSystemResource(resourceName);
+        String configPath = URLDecoder.decode(resource.getFile(), "UTF-8");
 
-		ClassLoader classLoader = getClass().getClassLoader();
-		File file = new File(classLoader.getResource(resourceName).getFile());
-		String absolutePath = file.getAbsolutePath();
-		System.out.println(absolutePath);
-		assertTrue(absolutePath.endsWith(".png"));
-	}
+        //ClassLoader classLoader = getClass().getClassLoader();
+        File file = new File(configPath);
+        String absolutePath = file.getAbsolutePath();
+        System.out.println(absolutePath);
+        assertTrue(absolutePath.endsWith(".png"));
+        //QRCodeReader.decodeQRCode(file);
+        QRCodeReader.decodeQRCode(file);
+
+
+    }
 
 }
